f82bf1ec6f745ab0f0f0e227cc1cd36406b66be2
AndreyPiganov <andrey.piganov@mail.ru>
2024-09-18 22:13:45 +0300

    finish

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,6 +1,5 @@
 import * as admin from 'firebase-admin';
-import serviceAccount from './test-flame-ad0c6-firebase-adminsdk-4omfq-64de4b571f.json';
-
+import serviceAccount from './secret-key-firebase.json';
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
 });
